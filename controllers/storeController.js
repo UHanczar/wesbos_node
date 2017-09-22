a41cc01ad6a162fd481432f783d3eef8a274ae9f
UHanczar <u.hanczar@gmail.com>
2017-09-23 00:01:41 +0300

    Adding_tags_page

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -99,3 +99,14 @@ exports.getStoreBySlug = async (req, res) => {
   
   res.render('store', { store, title: store.name });
 };
+
+exports.getStoresByTag = async (req, res) => {
+  const tag = req.params.tag;
+  const tagQuery = tag || { $exists: true }
+  const tagsPromise = Store.getTagsList();
+  const storePromise = Store.find({ tags: tagQuery });
+
+  const [tags, stores] = await Promise.all([tagsPromise, storePromise]);
+
+  res.render('tags', { tags, title: 'Tags', tag, stores });
+};
--- models/Store.js
+++ models/Store.js
@@ -36,15 +36,32 @@ const storeSchema = new mongoose.Schema({
   photo: String
 });
 
-storeSchema.pre('save', function (next) {
+storeSchema.pre('save', async function (next) {
   if (!this.isModified('name')) {
     next(); // skip it
     return; // storp this function
   }
 
   this.slug = slug(this.name);
+
+  // if there is store with this name, so they should be store-1, store-2 and so on
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const storeWithSlug = await this.constructor.find({ slug: slugRegEx });
+
+  if (storeWithSlug.length) {
+    this.slug = `${this.slug}-${this.slug.length + 1}`;
+  }
+
   next();
   // later make it more resilient, so it has unique keys
 });
+
+storeSchema.statics.getTagsList = function() {
+  return this.aggregate([
+    { $unwind: '$tags' },
+    { $group: { _id: '$tags', count: { $sum: 1 } } },
+    { $sort: { count: -1 } }
+  ]);
+};
 
 module.exports = mongoose.model('Store', storeSchema);
